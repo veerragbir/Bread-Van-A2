3d721a07b8fcee1c31337717556f789fa04b03d7
veerragbir <115950138+veerragbir@users.noreply.github.com>
2025-10-21 19:37:32 -0400

    update models/init.py exports for new structure
    Added __all__ for explicit module exports
    
    Ensured all models (User, Resident, Driver, DriverSchedule, StopRequest, StopRequestStatus) are imported properly

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -1,3 +1,10 @@
-from .user import User, Resident, Driver, UserType
-from .stop_request import StopRequest, StopRequestStatus
+from .user import User, UserType, Resident, Driver
 from .driver_schedule import DriverSchedule
+from .stop_request import StopRequest, StopRequestStatus
+
+# Since there are multiple init files, we define __all__ here to specify what is imported
+__all__ = [
+    "User", "UserType", "Resident", "Driver",
+    "DriverSchedule",
+    "StopRequest", "StopRequestStatus"
+]
