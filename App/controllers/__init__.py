f0ad5c6122301fe808d86703ab7da0e281bc5f1a
veerragbir <115950138+veerragbir@users.noreply.github.com>
2025-10-21 19:38:34 -0400

    clean controller imports and add explicit exports
    Added __all__ for better namespace management
    
    Cleaned import structure to match updated controllers

--- App/controllers/__init__.py
+++ App/controllers/__init__.py
@@ -1,8 +1,16 @@
-from .user import *
-from .auth import *
-from .initialize import *
-# Bread Van App Controllers
-from .user import UserController
-from .stop_request import StopRequestController
+from .user import UserController, create_user, get_all_users, get_all_users_json
+from .stop_request import StopRequestController, create_stop_request, get_stop_requests_by_resident
 from .schedule import ScheduleController
 from .location import LocationController
+from .initialize import initialize
+from .auth import login, logout  # if present
+
+
+# Since there are multiple init files, we define __all__ here to specify what is imported
+__all__ = [
+    "UserController", "create_user", "get_all_users", "get_all_users_json",
+    "StopRequestController", "create_stop_request", "get_stop_requests_by_resident",
+    "ScheduleController", "LocationController",
+    "initialize",
+    "login", "logout"
+]
